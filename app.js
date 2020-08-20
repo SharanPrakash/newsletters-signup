510ce822b5362934130c446df211c300ee958e24
Sharan Prakash <sharanprakash.sfc@gmail.com>
2020-08-20 14:02:09 +0530

    Complete post to mailchimp

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const port = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post("/", function(req, res) {
+    var firstName = req.body.fname;
+    var secondName = req.body.lname;
+    var emailId = req.body.email;
+    var data = {
+      members: [{
+        email_address: emailId,
+        status: "subscribed",
+        FNAME: firstName,
+        LNAME: secondName
+      }]
+    };
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+      url: "https://us17.api.mailchimp.com/3.0/lists/d0169a23d6",
+      method: "POST",
+      headers: {
+        "Authorization": "testing 59ff38e847e8a3c9765d5fe5a643023e-us17"
+      },
+      body: jsonData
+    };
+    request(options, function(error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(response.statusCode);
+      };
+    });
+});
+
+
+app.listen(port, function(req, res) {
+  console.log("Server is running on port 3000");
+});
+
+
+//59ff38e847e8a3c9765d5fe5a643023e-us17
+
+//d0169a23d6
